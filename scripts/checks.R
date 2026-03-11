eaf06d3227dac3723fe73d0ebcf37da5e1e25a3a
Jeanine13 <jeanined@hotmail.co.uk>
2026-03-11 22:03:15 +0000

    Add ChIPseeker annotation, GO/KEGG enrichment and MOFA2 setup scripts

---
+++ scripts/checks.R
@@ -0,0 +1,7 @@
+#checking if used the correct bulk counts
+
+verify_oligo_bulk_vst <- read.csv("/scratch/prj/bcn_marzi_lab/analysis_cutandtag_pd_bulk/student_data_package/data_in/vst_norm_peak_counts/20260227_oligo_noage_vst_peak_counts_N99464.csv")
+#im using the correct vst 
+
+#load the norm count 
+verify_oligo_bulk_countnorm <- read.csv("/scratch/prj/bcn_marzi_lab/analysis_cutandtag_pd_bulk/student_data_package/data_in/count_norm_peak_counts/20260227_oligo_noage_norm_peak_counts_N99464.csv")
