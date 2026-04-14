9141e86c407dbc107d9c21edb2c6295549d6226c
Jeanine13 <jeanined@hotmail.co.uk>
2026-04-14 17:01:32 +0100

    Clean MOFA pipeline - rGREAT only for GO enrichment, updated ChIPseeker annotation script with save functionality

--- scripts/cluster_peaks_chipseeker.R
+++ scripts/cluster_peaks_chipseeker.R
@@ -1,9 +1,18 @@
+# CHIPSEEKER ANNOTATION SCRIPT
+# RUN THIS SCRIPT WHEN PEAK OBJECTS ARE UPDATED
+# CREATES ANNOTATION OBJECTS FOR ALL FOUR DATASETS WITH PEAK KEYS AND GENE SYMBOLS
+# USED LATER FOR MOFA WEIGHT INTERPRETATION
 
-#GO FOR SC DATA RE-CALLED ON CLUSTERS
-#chip seeker annoates each peak with genomic location and nearest gene 
-#annoate peak sssigns each peak a genic location and add_gene_symbols adds
-# human readable gene symbols as TxDb only gives EntrezIDs. Uses ensDb to add gene names
+.libPaths(c(
+  "/cephfs/volumes/hpc_data_usr/k25093549/eabe5dc4-1fa9-4cdc-b2af-6a4d37d00142/R/R/x86_64-pc-linux-gnu-library/4.5",
+  .libPaths()
+))
 
+# DEFINE OUTPUT DIRECTORY
+mofa_out_dir <- "/scratch/prj/bcn_marzi_lab/analysis_cutandtag_pd_sc/student_data_package/jd_analysis_sc/saved_objects/new_sc_mofa_clean_14042026"
+dir.create(mofa_out_dir, recursive = TRUE, showWarnings = FALSE)
+
+# LOAD GRANGES AND VST DATA
 load("/scratch/prj/bcn_marzi_lab/analysis_cutandtag_pd_sc/student_data_package/jd_analysis_sc/saved_objects/mofa_OLIGO_clusterpeaks_granges_vst.RData")
 
 library(ChIPseeker)
@@ -11,112 +20,124 @@ library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(EnsDb.Hsapiens.v86)
 library(AnnotationDbi)
 library(dplyr)
+library(GenomeInfoDb)
 
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 
-# fix chromosome naming
+# FIX CHROMOSOME NAMING TO MATCH TXDB
+seqlevelsStyle(bulk_gr)    <- "UCSC"
 seqlevelsStyle(opalin_gr)  <- "UCSC"
 seqlevelsStyle(plekhg1_gr) <- "UCSC"
 seqlevelsStyle(opc_gr)     <- "UCSC"
 
-# annotate
-opalin_anno_cp  <- as.data.frame(annotatePeak(opalin_gr,  tssRegion = c(-3000, 3000), TxDb = txdb, annoDb = "org.Hs.eg.db"))
-plekhg1_anno_cp <- as.data.frame(annotatePeak(plekhg1_gr, tssRegion = c(-3000, 3000), TxDb = txdb, annoDb = "org.Hs.eg.db"))
-opc_anno_cp     <- as.data.frame(annotatePeak(opc_gr,     tssRegion = c(-3000, 3000), TxDb = txdb, annoDb = "org.Hs.eg.db"))
+# ANNOTATE ALL PEAKS WITH GENIC LOCATIONS AND NEAREST GENE (ENTREZID)
+bulk_anno_df <- as.data.frame(
+  annotatePeak(
+    bulk_gr,
+    tssRegion = c(-3000, 3000),
+    TxDb = txdb,
+    annoDb = "org.Hs.eg.db"
+  )
+)
 
-# add gene symbols
+opalin_anno_cp <- as.data.frame(
+  annotatePeak(
+    opalin_gr,
+    tssRegion = c(-3000, 3000),
+    TxDb = txdb,
+    annoDb = "org.Hs.eg.db"
+  )
+)
+
+plekhg1_anno_cp <- as.data.frame(
+  annotatePeak(
+    plekhg1_gr,
+    tssRegion = c(-3000, 3000),
+    TxDb = txdb,
+    annoDb = "org.Hs.eg.db"
+  )
+)
+
+opc_anno_cp <- as.data.frame(
+  annotatePeak(
+    opc_gr,
+    tssRegion = c(-3000, 3000),
+    TxDb = txdb,
+    annoDb = "org.Hs.eg.db"
+  )
+)
+
+# ADD HUMAN-READABLE GENE SYMBOLS AND GENE NAMES USING ENSDB
 add_gene_symbols <- function(anno_df) {
-  annotations_edb <- AnnotationDbi::select(EnsDb.Hsapiens.v86,
-                                           keys    = anno_df$geneId,
-                                           columns = c("GENENAME"),
-                                           keytype = "ENTREZID")
+  annotations_edb <- AnnotationDbi::select(
+    EnsDb.Hsapiens.v86,
+    keys = as.character(anno_df$geneId),
+    columns = c("SYMBOL", "GENENAME"),
+    keytype = "ENTREZID"
+  )
+  
   annotations_edb$ENTREZID <- as.character(annotations_edb$ENTREZID)
   annotations_edb <- annotations_edb[!duplicated(annotations_edb$ENTREZID), ]
-  anno_df %>% left_join(annotations_edb, by = c("geneId" = "ENTREZID"))
+  
+  anno_df %>%
+    left_join(annotations_edb, by = c("geneId" = "ENTREZID"))
 }
 
+bulk_anno_df    <- add_gene_symbols(bulk_anno_df)
 opalin_anno_cp  <- add_gene_symbols(opalin_anno_cp)
 plekhg1_anno_cp <- add_gene_symbols(plekhg1_anno_cp)
 opc_anno_cp     <- add_gene_symbols(opc_anno_cp)
 
-
-head(opalin_anno_cp[, c("seqnames", "start", "end", "annotation", "GENENAME.x")])
-
-colnames(opalin_anno_cp)
-head(opalin_anno_cp[, c("seqnames", "start", "end", "annotation", "GENENAME.x")])
-
-
 # CREATE PEAK KEYS FOR MERGING WITH MOFA WEIGHTS
-bulk_anno_df$peak_key   <- paste(bulk_anno_df$seqnames, bulk_anno_df$start, bulk_anno_df$end, sep = "_")
-opalin_anno_cp$peak_key <- paste(gsub("chr", "", opalin_anno_cp$seqnames),
-                                 opalin_anno_cp$start,
-                                 opalin_anno_cp$end, sep = "-")
+# BULK PEAKS ARE IN chr1_start_end FORMAT
+# SC PEAKS ARE IN 1-start-end FORMAT
+bulk_anno_df$peak_key <- paste(
+  bulk_anno_df$seqnames,
+  bulk_anno_df$start,
+  bulk_anno_df$end,
+  sep = "_"
+)
 
-# ADD PEAK KEYS FOR PLEKHG1 AND OPC - NEEDED FOR MOFA WEIGHT ANNOTATION (forgot these before)
-plekhg1_anno_cp$peak_key <- paste(gsub("chr", "", plekhg1_anno_cp$seqnames),
-                                  plekhg1_anno_cp$start,
-                                  plekhg1_anno_cp$end, sep = "-")
+opalin_anno_cp$peak_key <- paste(
+  gsub("chr", "", opalin_anno_cp$seqnames),
+  opalin_anno_cp$start,
+  opalin_anno_cp$end,
+  sep = "-"
+)
 
-opc_anno_cp$peak_key <- paste(gsub("chr", "", opc_anno_cp$seqnames),
-                              opc_anno_cp$start,
-                              opc_anno_cp$end, sep = "-")
+plekhg1_anno_cp$peak_key <- paste(
+  gsub("chr", "", plekhg1_anno_cp$seqnames),
+  plekhg1_anno_cp$start,
+  plekhg1_anno_cp$end,
+  sep = "-"
+)
 
+opc_anno_cp$peak_key <- paste(
+  gsub("chr", "", opc_anno_cp$seqnames),
+  opc_anno_cp$start,
+  opc_anno_cp$end,
+  sep = "-"
+)
 
+# VERIFY ANNOTATION OBJECTS
+cat("Bulk annotation peaks:", nrow(bulk_anno_df), "\n")
+cat("Opalin annotation peaks:", nrow(opalin_anno_cp), "\n")
+cat("Plekhg1 annotation peaks:", nrow(plekhg1_anno_cp), "\n")
+cat("OPC annotation peaks:", nrow(opc_anno_cp), "\n")
 
-#can proceed with mofa after running chipseeker annotations, GO can be done after mofa. MOVE ONTO MOFA. 
+# OPTIONAL PREVIEW OF KEY COLUMNS
+cat("\nPreview of Opalin annotation:\n")
+print(head(opalin_anno_cp[, c("seqnames", "start", "end", "annotation", "SYMBOL", "GENENAME", "peak_key")]))
 
+# SAVE FOR DOWNSTREAM MOFA USE
+save(
+  bulk_anno_df,
+  opalin_anno_cp,
+  plekhg1_anno_cp,
+  opc_anno_cp,
+  file = file.path(mofa_out_dir, "chipseeker_annotations_all_datasets.RData")
+)
 
-
-
-
-#IGNORE FOR NOW
-
-#GO ENRICHMENT , issues with this, need to look over
-library(clusterProfiler)
-library(ggplot2)
-
-out_dir <- "/scratch/prj/bcn_marzi_lab/analysis_cutandtag_pd_sc/student_data_package/jd_analysis_sc/saved_objects/oligo_data_out"
-
-# function to get entrez IDs from unique SC peaks
-get_entrez_sc <- function(anno_df, unique_peaks) {
-  key <- paste(gsub("chr", "", anno_df$seqnames), anno_df$start, anno_df$end, sep = "-")
-  entrez <- unique(anno_df$geneId[key %in% unique_peaks])
-  entrez[!is.na(entrez)]
-}
-
-# extract entrez IDs for SC unique peaks
-opalin_unique_entrez  <- get_entrez_sc(opalin_anno_cp,  opalin_unique_sc)
-plekhg1_unique_entrez <- get_entrez_sc(plekhg1_anno_cp, plekhg1_unique_sc)
-opc_unique_entrez     <- get_entrez_sc(opc_anno_cp,     opc_unique_sc)
-
-cat("Opalin+ unique genes:", length(opalin_unique_entrez), "\n")
-cat("Plekhg1+ unique genes:", length(plekhg1_unique_entrez), "\n")
-cat("OPC unique genes:", length(opc_unique_entrez), "\n")
-
-
-# run GO enrichment function
-run_go <- function(entrez, label) {
-  ego <- enrichGO(gene          = entrez,
-                  keyType       = "ENTREZID",
-                  OrgDb         = org.Hs.eg.db,
-                  ont           = "BP",
-                  pAdjustMethod = "BH",
-                  qvalueCutoff  = 0.05,
-                  readable      = TRUE)
-  
-  write.csv(data.frame(ego),
-            file.path(out_dir, paste0("GO_clusterpeaks_", label, "_sc_unique.csv")),
-            row.names = FALSE)
-  
-  ggsave(file.path(out_dir, paste0("dotplot_GO_clusterpeaks_", label, "_sc_unique.png")),
-         plot = dotplot(ego, showCategory = 30) +
-           labs(title = paste("GO -", label, "SC unique peaks")),
-         width = 10, height = 14, dpi = 150)
-  
-  ego
-}
-
-ego_opalin  <- run_go(opalin_unique_entrez,  "opalin")
-ego_plekhg1 <- run_go(plekhg1_unique_entrez, "plekhg1")
-ego_opc     <- run_go(opc_unique_entrez,     "opc")
+cat("\nChIPseeker annotation complete and saved.\n")
+cat("Saved file:", file.path(mofa_out_dir, "chipseeker_annotations_all_datasets.RData"), "\n")
 
